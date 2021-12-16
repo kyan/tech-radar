1c5d4630d9edb3c14b64c0b009774f37cb64a5cd
dquilter <david@kyan.com>
2021-12-16 10:17:15 +0000

    Don't ESLint

--- src/util/factory.js
+++ src/util/factory.js
@@ -1,321 +1,295 @@
 /* eslint no-constant-condition: "off" */
 
-const d3 = require("d3");
+const d3 = require('d3')
 const _ = {
-  map: require("lodash/map"),
-  uniqBy: require("lodash/uniqBy"),
-  capitalize: require("lodash/capitalize"),
-  each: require("lodash/each"),
-};
+  map: require('lodash/map'),
+  uniqBy: require('lodash/uniqBy'),
+  capitalize: require('lodash/capitalize'),
+  each: require('lodash/each')
+}
 
-const InputSanitizer = require("./inputSanitizer");
-const Radar = require("../models/radar");
-const Quadrant = require("../models/quadrant");
-const Ring = require("../models/ring");
-const Blip = require("../models/blip");
-const GraphingRadar = require("../graphing/radar");
-const QueryParams = require("./queryParamProcessor");
-const MalformedDataError = require("../exceptions/malformedDataError");
-const SheetNotFoundError = require("../exceptions/sheetNotFoundError");
-const ContentValidator = require("./contentValidator");
-const Sheet = require("./sheet");
-const ExceptionMessages = require("./exceptionMessages");
-const GoogleAuth = require("./googleAuth");
+const InputSanitizer = require('./inputSanitizer')
+const Radar = require('../models/radar')
+const Quadrant = require('../models/quadrant')
+const Ring = require('../models/ring')
+const Blip = require('../models/blip')
+const GraphingRadar = require('../graphing/radar')
+const QueryParams = require('./queryParamProcessor')
+const MalformedDataError = require('../exceptions/malformedDataError')
+const SheetNotFoundError = require('../exceptions/sheetNotFoundError')
+const ContentValidator = require('./contentValidator')
+const Sheet = require('./sheet')
+const ExceptionMessages = require('./exceptionMessages')
+const GoogleAuth = require('./googleAuth')
 const Papa = require("papaparse");
 
-const sheetUrl =
-  "https://docs.google.com/spreadsheets/d/1Qle0jpHR81-GHH_FPU2uCkdxYRZfFGjBY5aCTg36Lc0";
+const sheetUrl = 'https://docs.google.com/spreadsheets/d/1Qle0jpHR81-GHH_FPU2uCkdxYRZfFGjBY5aCTg36Lc0';
 
 const plotRadar = function (title, blips, currentRadarName, alternativeRadars) {
-  if (title.endsWith(".csv")) {
-    title = title.substring(0, title.length - 4);
+  if (title.endsWith('.csv')) {
+    title = title.substring(0, title.length - 4)
   }
-  document.title = title;
-  d3.selectAll(".loading").remove();
+  document.title = title
+  d3.selectAll('.loading').remove()
 
-  var rings = _.map(_.uniqBy(blips, "ring"), "ring");
-  var ringMap = {};
-  var maxRings = 5;
+  var rings = _.map(_.uniqBy(blips, 'ring'), 'ring')
+  var ringMap = {}
+  var maxRings = 5
 
   _.each(rings, function (ringName, i) {
     if (i === maxRings) {
-      throw new MalformedDataError(ExceptionMessages.TOO_MANY_RINGS);
+      throw new MalformedDataError(ExceptionMessages.TOO_MANY_RINGS)
     }
-    ringMap[ringName] = new Ring(ringName, i);
-  });
+    ringMap[ringName] = new Ring(ringName, i)
+  })
 
-  var quadrants = {};
+  var quadrants = {}
   _.each(blips, function (blip) {
     if (!quadrants[blip.quadrant]) {
-      quadrants[blip.quadrant] = new Quadrant(_.capitalize(blip.quadrant));
+      quadrants[blip.quadrant] = new Quadrant(_.capitalize(blip.quadrant))
     }
-    quadrants[blip.quadrant].add(
-      new Blip(
-        blip.name,
-        ringMap[blip.ring],
-        blip.department,
-        blip.status,
-        blip.topic,
-        blip.description
-      )
-    );
-  });
+    quadrants[blip.quadrant].add(new Blip(blip.name, ringMap[blip.ring], blip.department, blip.status, blip.topic, blip.description))
+  })
 
-  var radar = new Radar();
+  var radar = new Radar()
   _.each(quadrants, function (quadrant) {
-    radar.addQuadrant(quadrant);
-  });
+    radar.addQuadrant(quadrant)
+  })
 
   if (alternativeRadars !== undefined || true) {
     alternativeRadars.forEach(function (sheetName) {
-      radar.addAlternative(sheetName);
-    });
+      radar.addAlternative(sheetName)
+    })
   }
 
   if (currentRadarName !== undefined || true) {
-    radar.setCurrentSheet(currentRadarName);
+    radar.setCurrentSheet(currentRadarName)
   }
 
-  var size = window.innerHeight - 100 < 620 ? 620 : window.innerHeight - 100;
+  var size = (window.innerHeight - 100) < 620 ? 620 : window.innerHeight - 100
 
-  new GraphingRadar(size, radar).init().plot();
-};
+  new GraphingRadar(size, radar).init().plot()
+}
 
 const GoogleSheet = function (sheetReference, sheetName) {
-  var self = {};
+  var self = {}
 
   self.build = function () {
-    Papa.parse(`${sheetReference}/pub?output=csv`, {
-      download: true,
-      header: true,
-      complete: createBlips,
-      error: handleSheetError,
-    });
+    Papa.parse(
+      `${sheetReference}/pub?output=csv`,
+      {
+        download: true,
+        header: true,
+        complete: createBlips,
+        error: handleSheetError,
+      }
+    );
 
-    function handleSheetError() {
-      plotErrorMessage(
-        new SheetNotFoundError(ExceptionMessages.SHEET_NOT_FOUND)
-      );
-      return;
+    function handleSheetError () {
+      plotErrorMessage(new SheetNotFoundError(ExceptionMessages.SHEET_NOT_FOUND))
+      return
     }
 
-    function createBlips(results) {
+    function createBlips (results) {
       try {
         if (!sheetName) {
-          sheetName = "Tech Radar";
+          sheetName = 'Tech Radar'
         }
-        var columnNames = results.meta.fields;
+        var columnNames = results.meta.fields
 
-        var contentValidator = new ContentValidator(columnNames);
-        contentValidator.verifyContent();
-        contentValidator.verifyHeaders();
+        var contentValidator = new ContentValidator(columnNames)
+        contentValidator.verifyContent()
+        contentValidator.verifyHeaders()
 
-        var all = results.data;
-        var blips = _.map(all, new InputSanitizer().sanitize);
+        var all = results.data
+        var blips = _.map(all, new InputSanitizer().sanitize)
 
-        plotRadar(sheetName, blips, sheetName, []);
+        plotRadar(sheetName, blips, sheetName, [])
       } catch (exception) {
-        plotErrorMessage(exception);
+        plotErrorMessage(exception)
       }
     }
-  };
+  }
 
-  function createBlipsForProtectedSheet(documentTitle, values, sheetNames) {
+  function createBlipsForProtectedSheet (documentTitle, values, sheetNames) {
     if (!sheetName) {
-      sheetName = sheetNames[0];
+      sheetName = sheetNames[0]
     }
     values.forEach(function (value) {
-      var contentValidator = new ContentValidator(values[0]);
-      contentValidator.verifyContent();
-      contentValidator.verifyHeaders();
-    });
+      var contentValidator = new ContentValidator(values[0])
+      contentValidator.verifyContent()
+      contentValidator.verifyHeaders()
+    })
 
-    const all = values;
-    const header = all.shift();
-    var blips = _.map(all, (blip) =>
-      new InputSanitizer().sanitizeForProtectedSheet(blip, header)
-    );
-    plotRadar(documentTitle + " - " + sheetName, blips, sheetName, sheetNames);
+    const all = values
+    const header = all.shift()
+    var blips = _.map(all, blip => new InputSanitizer().sanitizeForProtectedSheet(blip, header))
+    plotRadar(documentTitle + ' - ' + sheetName, blips, sheetName, sheetNames)
   }
 
   self.authenticate = function (force = false, callback) {
     GoogleAuth.loadGoogle(function (e) {
-      GoogleAuth.login((_) => {
-        var sheet = new Sheet(sheetReference);
-        sheet.processSheetResponse(
-          sheetName,
-          createBlipsForProtectedSheet,
-          (error) => {
-            if (error.status === 403) {
-              plotUnauthorizedErrorMessage();
-            } else {
-              plotErrorMessage(error);
-            }
+      GoogleAuth.login(_ => {
+        var sheet = new Sheet(sheetReference)
+        sheet.processSheetResponse(sheetName, createBlipsForProtectedSheet, error => {
+          if (error.status === 403) {
+            plotUnauthorizedErrorMessage()
+          } else {
+            plotErrorMessage(error)
           }
-        );
-        if (callback) {
-          callback();
-        }
-      }, force);
-    });
-  };
-
-  self.init = function () {
-    plotLoading();
-    return self;
-  };
-
-  return self;
-};
-
-const GoogleSheetInput = function () {
-  var self = {};
-  var sheet;
-
-  self.build = function () {
-    var queryString = window.location.href.match(/sheetName(.*)/);
-    var queryParams = queryString ? QueryParams(queryString[0]) : {};
-
-    sheet = GoogleSheet(sheetUrl, queryParams.sheetName);
-    sheet.init().build();
-  };
-
-  return self;
-};
-
-function plotLoading(content) {
-  content = d3
-    .select("body")
-    .append("div")
-    .attr("class", "loading")
-    .append("div")
-    .attr("class", "loading__wrap");
-
-  plotLoadingMessage(content);
-}
-
-function plotLogo(content) {
-  content
-    .append("div")
-    .attr("class", "input-sheet__logo")
-    .html(
-      '<a href="https://www.thoughtworks.com"><img src="/images/tw-logo.png" / ></a>'
-    );
-}
-
-function plotLoadingMessage(content) {
-  content
-    .append("div")
-    .attr("class", "loading__wrap")
-    .append("div")
-    .attr("class", "loading__logo")
-    .html('<img src="/images/kyanWhite.svg" />');
-
-  content
-    .select(".loading__wrap")
-    .append("div")
-    .attr("class", "loading__title")
-    .append("h1")
-    .text("Tech Radar");
-}
-
-function plotBanner(content, text) {
-  content.append("div").attr("class", "input-sheet__banner").html(text);
-}
-
-function plotErrorMessage(exception) {
-  var message =
-    "Oops! It seems like there are some problems with loading your data. ";
-
-  var content = d3.select("body").append("div").attr("class", "error-page");
-
-  content
-    .append("div")
-    .attr("class", "error-page__wrap")
-    .append("div")
-    .attr("class", "error-page__logo")
-    .html('<img src="/images/kyanWhite.svg" />');
-
-  content
-    .select(".error-page__wrap")
-    .append("div")
-    .attr("class", "error-page__title")
-    .append("h1")
-    .text("Tech Radar");
-
-  d3.selectAll(".loading").remove();
-
-  if (exception instanceof MalformedDataError) {
-    message = message.concat(exception.message);
-  } else if (exception instanceof SheetNotFoundError) {
-    message = exception.message;
-  } else if (exception instanceof RangeError) {
-    message =
-      "Your window is too small to render the data. Please enlarge the window size and refresh.";
-  } else {
-    console.error(exception);
+        })
+        if (callback) { callback() }
+      }, force)
+    })
   }
 
-  const container = content.append("div").attr("class", "error-container");
-  var errorContainer = container
-    .append("div")
-    .attr("class", "error-container__message");
-  errorContainer.append("div").append("p").html(message);
+  self.init = function () {
+    plotLoading()
+    return self
+  }
 
-  var homePageURL = window.location.protocol + "//" + window.location.hostname;
-  homePageURL += window.location.port === "" ? "" : ":" + window.location.port;
-  var homePage = "<a href=" + homePageURL + ">GO BACK</a>";
-
-  errorContainer.append("div").append("p").html(homePage);
+  return self
 }
 
-function plotUnauthorizedErrorMessage() {
-  var content = d3.select("body").append("div").attr("class", "input-sheet");
+const GoogleSheetInput = function () {
+  var self = {}
+  var sheet
 
-  plotLogo(content);
+  self.build = function () {
+    var queryString = window.location.href.match(/sheetName(.*)/)
+    var queryParams = queryString ? QueryParams(queryString[0]) : {}
 
-  var bannerText = "<div><h1>Build your own radar</h1></div>";
+    sheet = GoogleSheet(sheetUrl, queryParams.sheetName)
+    sheet.init().build()
+  }
 
-  plotBanner(content, bannerText);
-
-  d3.selectAll(".loading").remove();
-  const currentUser = GoogleAuth.geEmail();
-  let homePageURL = window.location.protocol + "//" + window.location.hostname;
-  homePageURL += window.location.port === "" ? "" : ":" + window.location.port;
-  const goBack = "<a href=" + homePageURL + ">GO BACK</a>";
-  const message = `<strong>Oops!</strong> Looks like you are accessing this sheet using <b>${currentUser}</b>, which does not have permission.Try switching to another account.`;
-
-  const container = content.append("div").attr("class", "error-container");
-
-  const errorContainer = container
-    .append("div")
-    .attr("class", "error-container__message");
-
-  errorContainer
-    .append("div")
-    .append("p")
-    .attr("class", "error-title")
-    .html(message);
-
-  const button = errorContainer
-    .append("button")
-    .attr("class", "button switch-account-button")
-    .text("SWITCH ACCOUNT");
-
-  errorContainer
-    .append("div")
-    .append("p")
-    .attr("class", "error-subtitle")
-    .html(`or ${goBack} to try a different sheet.`);
-
-  button.on("click", (_) => {
-    var queryString = window.location.href.match(/sheetName(.*)/);
-    var queryParams = queryString ? QueryParams(queryString[0]) : {};
-    const sheet = GoogleSheet(sheetUrl, queryParams.sheetName);
-    sheet.authenticate(true, (_) => {
-      content.remove();
-    });
-  });
+  return self
 }
 
-module.exports = GoogleSheetInput;
+function plotLoading (content) {
+  content = d3.select('body')
+    .append('div')
+    .attr('class', 'loading')
+    .append('div')
+    .attr('class', 'loading__wrap')
+
+  plotLoadingMessage(content)
+}
+
+function plotLogo (content) {
+  content.append('div')
+    .attr('class', 'input-sheet__logo')
+    .html('<a href="https://www.thoughtworks.com"><img src="/images/tw-logo.png" / ></a>')
+}
+
+function plotLoadingMessage (content) {
+  content.append('div')
+    .attr('class', 'loading__wrap')
+    .append('div')
+    .attr('class', 'loading__logo')
+    .html('<img src="/images/kyanWhite.svg" />')
+
+  content.select('.loading__wrap')
+    .append('div')
+    .attr('class', 'loading__title')
+    .append('h1')
+    .text('Tech Radar')
+}
+
+function plotBanner (content, text) {
+  content.append('div')
+    .attr('class', 'input-sheet__banner')
+    .html(text)
+}
+
+function plotErrorMessage (exception) {
+  var message = 'Oops! It seems like there are some problems with loading your data. '
+
+  var content = d3.select('body')
+    .append('div')
+    .attr('class', 'error-page')
+
+  content.append('div')
+    .attr('class', 'error-page__wrap')
+    .append('div')
+    .attr('class', 'error-page__logo')
+    .html('<img src="/images/kyanWhite.svg" />')
+
+  content.select('.error-page__wrap')
+    .append('div')
+    .attr('class', 'error-page__title')
+    .append('h1')
+    .text('Tech Radar')
+
+  d3.selectAll('.loading').remove()
+
+  if (exception instanceof MalformedDataError) {
+    message = message.concat(exception.message)
+  } else if (exception instanceof SheetNotFoundError) {
+    message = exception.message
+  } else if (exception instanceof RangeError) {
+    message = "Your window is too small to render the data. Please enlarge the window size and refresh."
+  } else {
+    console.error(exception)
+  }
+
+  const container = content.append('div').attr('class', 'error-container')
+  var errorContainer = container.append('div')
+    .attr('class', 'error-container__message')
+  errorContainer.append('div').append('p')
+    .html(message)
+
+  var homePageURL = window.location.protocol + '//' + window.location.hostname
+  homePageURL += (window.location.port === '' ? '' : ':' + window.location.port)
+  var homePage = '<a href=' + homePageURL + '>GO BACK</a>'
+
+  errorContainer.append('div').append('p')
+    .html(homePage)
+}
+
+function plotUnauthorizedErrorMessage () {
+  var content = d3.select('body')
+    .append('div')
+    .attr('class', 'input-sheet')
+
+  plotLogo(content)
+
+  var bannerText = '<div><h1>Build your own radar</h1></div>'
+
+  plotBanner(content, bannerText)
+
+  d3.selectAll('.loading').remove()
+  const currentUser = GoogleAuth.geEmail()
+  let homePageURL = window.location.protocol + '//' + window.location.hostname
+  homePageURL += (window.location.port === '' ? '' : ':' + window.location.port)
+  const goBack = '<a href=' + homePageURL + '>GO BACK</a>'
+  const message = `<strong>Oops!</strong> Looks like you are accessing this sheet using <b>${currentUser}</b>, which does not have permission.Try switching to another account.`
+
+  const container = content.append('div').attr('class', 'error-container')
+
+  const errorContainer = container.append('div')
+    .attr('class', 'error-container__message')
+
+  errorContainer.append('div').append('p')
+    .attr('class', 'error-title')
+    .html(message)
+
+  const button = errorContainer.append('button')
+    .attr('class', 'button switch-account-button')
+    .text('SWITCH ACCOUNT')
+
+  errorContainer.append('div').append('p')
+    .attr('class', 'error-subtitle')
+    .html(`or ${goBack} to try a different sheet.`)
+
+  button.on('click', _ => {
+    var queryString = window.location.href.match(/sheetName(.*)/)
+    var queryParams = queryString ? QueryParams(queryString[0]) : {}
+    const sheet = GoogleSheet(sheetUrl, queryParams.sheetName)
+    sheet.authenticate(true, _ => {
+      content.remove()
+    })
+  })
+}
+
+module.exports = GoogleSheetInput
